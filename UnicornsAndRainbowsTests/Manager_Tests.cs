2932db34b5134b366657189c5407d820b3c8f924
Eric Gunnerson <eric.gunnerson@microsoft.com>
2015-10-20 15:25:18 -0700

    Added a test to verify that the current SearchRoot value is used.

--- UnicornsAndRainbowsTests/Manager_Tests.cs
+++ UnicornsAndRainbowsTests/Manager_Tests.cs
@@ -24,5 +24,23 @@ namespace UnicornsAndRainbowsTests
             Assert.AreEqual(configuration.SearchRoot + "Puppies", browserNavigator.Uri.OriginalString);
             Assert.AreEqual(1, userInterface.Count);
         }
+
+        [TestMethod]
+        public void when_I_change_the_searchRoot_and_click_on_a_button__the_proper_page_is_launched_in_the_browser()
+        {
+            UserInterfaceSimulator userInterface = new UserInterfaceSimulator();
+
+            Configuration configuration = new Configuration();
+            configuration.SearchRoot = "http://www.example.com/search/term=";
+
+            BrowserNavigatorSimulator browserNavigator = new BrowserNavigatorSimulator();
+
+            Manager manager = new Manager(userInterface, configuration, browserNavigator);
+
+            configuration.SearchRoot = "http://www.example.com/makethesearch/term=";
+            userInterface.SimulateButtonClick("Puppies");
+            Assert.AreEqual(configuration.SearchRoot + "Puppies", browserNavigator.Uri.OriginalString);
+            Assert.AreEqual(1, userInterface.Count);
+        }
     }
 }
